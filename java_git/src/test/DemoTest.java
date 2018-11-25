8eac155adac9d81ddf195d16c01cdeecf3a009e1
HughTouch <m17858642107@163.com>
2018-11-25 17:14:36 +0800

    添加test工程至git

---
+++ java_git/src/test/DemoTest.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * @BelongsProject: java_git
+ * @BelongsPackage: test
+ * @Author: Mr.Huang
+ * @CreateTime: 2018-11-25 17:12
+ * @Description:
+ */
+public class DemoTest {
+    public static void main(String[] args) {
+        System.out.println(111);
+    }
+}
