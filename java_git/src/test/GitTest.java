7ea954fe9ddd352867ff8e944bc89a23e78e92c0
HughTouch <m17858642107@163.com>
2019-01-04 14:17:16 +0800

    添加test工程至git

---
+++ java_git/src/test/GitTest.java
@@ -0,0 +1,15 @@
+package test;
+
+/**
+ * @BelongsProject: java_git
+ * @BelongsPackage: test
+ * @Author: Mr.Huang
+ * @CreateTime: 2019-01-04 14:13
+ * @Description:
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
